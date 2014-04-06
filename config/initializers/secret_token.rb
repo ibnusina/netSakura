b505296c05beeebbdad3583143c507b9b08c6c5d
Ibnu <ibnu.sina009@gmail.com>
2014-04-06 23:13:51 +0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+NetSakura::Application.config.secret_key_base = '4e0106380ce2dc1fb7a4d298c05c51ed4659965ab4e74887c0145ca505182787836126aea3f2bad0350f5dc3ebbeb80887243523e94e8a072140fb9c7b7bd36c'
